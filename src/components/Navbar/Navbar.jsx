ad115f0be56a79e307316d0e36b772b4f7d72029
Daniks0-0 <your.email@example.com>
2024-12-03 17:43:14 +0300

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,72 +1,90 @@
 import React from "react";
-import {  NavLink, useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router";
+import { slide as Menu } from 'react-burger-menu'
 import "./Navbar.scss";
 import logo from "../../assets/mainlogo.svg";
+import openButton from "../../assets/openButton.svg";
 
 
 const Navbar = () => {
+
+    const CustomBurgerIcon = () => openButton;
+
     const navigate = useNavigate();
 
     //обработчки для "контакты"
-    const handleContactsClick = () =>{
+    const handleContactsClick = () => {
         navigate("/"); //переход на главную страницу
-         //скролл к разделу "Контакты"
-     setTimeout(() => {
-        const contactsSection = document.getElementById("contacts-id");
-        if (contactsSection) {
-            contactsSection.scrollIntoView({ behavior: "smooth" });
-        }
-    }, 100); 
-    }
-    
-    //обработчки для "Услуги"
-    const handleServicesClick = () =>{
-        navigate("/");
-         //скролл к разделу "Услуги"
-        setTimeout(()=>{
-            const servicesSection = document.getElementById("servicesSection-id");
-            if(servicesSection){
-                servicesSection.scrollIntoView({behavior: "smooth"});
+        //скролл к разделу "Контакты"
+        setTimeout(() => {
+            const contactsSection = document.getElementById("contacts-id");
+            if (contactsSection) {
+                contactsSection.scrollIntoView({ behavior: "smooth" });
             }
-        },100);
+        }, 100);
     }
-    
+
+    //обработчки для "Услуги"
+    const handleServicesClick = () => {
+        navigate("/");
+        //скролл к разделу "Услуги"
+        setTimeout(() => {
+            const servicesSection = document.getElementById("servicesSection-id");
+            if (servicesSection) {
+                servicesSection.scrollIntoView({ behavior: "smooth" });
+            }
+        }, 100); //100 задержка для прогрузки страницы
+    }
+
     return (
-        <nav>
-        <div>
-            <a href="/">
-            <img src={logo} alt="Энергострой" />
-            </a>
-            
-        </div>
-            <ul>
-                <li>
-                    <NavLink to="/" end className="link">
-                        Главная
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/projects" end className="link">
-                        Проекты
-                    </NavLink>
-                </li>
-                <li>
-                <button onClick={handleContactsClick} className="link">
-                        Контакты
-                    </button>
-                </li>
-                <li>
-                <button onClick={handleServicesClick} className="link">
-                        Услуги
-                    </button>
-                </li>
-             
-            </ul>
-            <div className="contacts__header">
-                <a href="tel:+74742710156">+7 (4742) 71-01-56</a>
-                <a href="mailto:energo_stroy48@bk.ru?subject=Здравствуйте! Я отправляю это письмо через сайт вашей компании.">energo_stroy48@bk.ru</a>
-            </div>
-        </nav>
+
+         <nav>
+                <div>
+                    <a href="/">
+                    <img src={logo} alt="Энергострой" />
+                    </a>
+
+                </div>
+                    <ul className="for-PC">
+                        <li>
+                            <NavLink to="/" end className="link">
+                                Главная
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/projects" end className="link">
+                                Проекты
+                            </NavLink>
+                        </li>
+                        <li>
+                        <a onClick={handleContactsClick} className="link">
+                                Контакты
+                         </a>
+
+                        </li>
+                        <li>
+                        <a onClick={handleServicesClick} className="link">
+                                Услуги
+                            </a>
+                        </li>
+
+                    </ul>
+                    <div className="contacts__header">
+                        <a href="tel:+74742710156">+7 (4742) 71-01-56</a>
+                        <a href="mailto:energo_stroy48@bk.ru?subject=Здравствуйте! Я отправляю это письмо через сайт вашей компании.">energo_stroy48@bk.ru</a>
+                    </div>
+                </nav> 
+
+         
+        
+       
+      
+
+
+
+
+
+
     );
 }
 
