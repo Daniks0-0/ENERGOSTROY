228a1434021cc1aada9ad559eef5bf69f84f2941
Daniks0-0 <your.email@example.com>
2024-12-09 20:52:26 +0300

    start mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router";
 import "./Navbar.scss";
 import logo from "../../assets/mainlogo.svg";
 
 
-
 const Navbar = () => {
-
-  
-
     const navigate = useNavigate();
-
+    const [menuOpen, setMenuOpen] = useState(false); // Состояние для управления меню
     //обработчки для "контакты"
     const handleContactsClick = () => {
         navigate("/"); //переход на главную страницу
@@ -21,8 +17,8 @@ const Navbar = () => {
                 contactsSection.scrollIntoView({ behavior: "smooth" });
             }
         }, 100);
+        setMenuOpen(false); //закрываем меню после нажатия на ссылку
     }
-
     //обработчки для "Услуги"
     const handleServicesClick = () => {
         navigate("/");
@@ -33,50 +29,64 @@ const Navbar = () => {
                 servicesSection.scrollIntoView({ behavior: "smooth" });
             }
         }, 100); //100 задержка для прогрузки страницы
+        setMenuOpen(false); //закрываем меню после нажатия на ссылку
+    }
+    //кнопка бургер меню
+    const handleBurgerClick = () => {
+        setMenuOpen(!menuOpen); // Переключение состояния меню
     }
 
     return (
-
-         <nav>
+        <div>
+            <button
+                className={`burger ${menuOpen ? "open" : ""}`}
+                id="button_id"
+                onClick={handleBurgerClick}
+            >
+                <div className="burger__line"></div>
+                <div className="burger__line"></div>
+                <div className="burger__line"></div>
+            </button>
+            <nav className={`menu ${menuOpen ? "open" : ""}`}>
                 <div>
                     <a href="/">
-                    <img src={logo} alt="Энергострой" />
+                        <img src={logo} alt="Энергострой" />
                     </a>
-
                 </div>
-                    <ul className="for-PC">
-                        <li>
-                            <NavLink to="/" end className="link">
-                                Главная
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/projects" end className="link">
-                                Проекты
-                            </NavLink>
-                        </li>
-                        <li>
+                <ul className={`for-PC ${menuOpen ? "active" : ""}`}>                     {/*добавляем класс active к элементу, если значение переменной menuOpen равно true. */}
+                    <li>
+                        <NavLink to="/" end className="link" onClick={() => setMenuOpen(false)}>
+                            Главная
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projects" end className="link" onClick={() => setMenuOpen(false)}>
+                            Проекты
+                        </NavLink>
+                    </li>
+                    <li>
                         <a onClick={handleContactsClick} className="link">
-                                Контакты
-                         </a>
-
-                        </li>
-                        <li>
+                            Контакты
+                        </a>
+                    </li>
+                    <li>
                         <a onClick={handleServicesClick} className="link">
-                                Услуги
-                            </a>
-                        </li>
-
-                    </ul>
-                    <div className="contacts__header">
-                        <a href="tel:+74742710156">+7 (4742) 71-01-56</a>
-                        <a href="mailto:energo_stroy48@bk.ru?subject=Здравствуйте! Я отправляю это письмо через сайт вашей компании.">energo_stroy48@bk.ru</a>
-                    </div>
-                </nav> 
-
-
-
+                            Услуги
+                        </a>
+                    </li>
+                </ul>
+                <div className="contacts__header">
+                    <a href="tel:+74742710156">+7 (4742) 71-01-56</a>
+                    <a href="mailto:energo_stroy48@bk.ru?subject=Здравствуйте! Я отправляю это письмо через сайт вашей компании.">
+                        energo_stroy48@bk.ru
+                    </a>
+                </div>
+            </nav>
+        </div>
     );
 }
+
+
+
 
 export default Navbar;
